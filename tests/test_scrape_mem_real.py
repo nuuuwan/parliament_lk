1b2867b2b9d4c8c795b9342b30c790b3e244cff7
nuuuwan <nuuuwan@gmail.com>
2022-04-19 11:42:44 +0530

    Tests pass for 3306 and 3449

--- tests/test_scrape_mem_real.py
+++ tests/test_scrape_mem_real.py
@@ -29,6 +29,23 @@ class TestCase(unittest.TestCase):
                 address_sitting=None,
                 email='wickremesinghe_r@parliament.lk',
             )],
+
+            [3306, dict(
+                name='Hon. Amarakeerthi Athukorala, M.P.',
+                party='Sri Lanka Podujana Peramuna (SLPP)',
+                electoral_district='Polonnaruwa',
+
+                date_of_birth='23-12-1964',
+                civil_status='Married',
+                religion='Buddhism',
+                profession=None,
+
+                phone='0773929655',
+                address='D S Senanayake Road,New Town,Polonnaruwa.',
+                phone_sitting='0272223282',
+                address_sitting=None,
+                email='amarakeerthi_a@parliament.lk',
+            )],
             [3438, dict(
                 name='Hon. Yadamini Gunawardena, M.P.',
                 party='Sri Lanka Podujana Peramuna (SLPP)',
@@ -44,7 +61,24 @@ class TestCase(unittest.TestCase):
                 phone_sitting='0714311163',
                 address_sitting='No. 84,Kirulapona Avenue,Colombo 05.',
                 email='yadamini_g@parliament.lk',
-            )]
+            )],
+            [3449, dict(
+                name='Hon. (Dr.) (Ms.) Harini Amarasuriya, M.P.',
+                party='Jathika Jana balawegaya (JJB)',
+                electoral_district='National List',
+
+                date_of_birth='06-03-1970',
+                civil_status='Single',
+                religion='Buddhism',
+                profession=None,
+
+                phone=None,
+                address='No. 33B" Janatha Mawatha,Mirihana,Kotte.',
+                phone_sitting='0112829722',
+                address_sitting='No. 464/20, Pannipitiya Road,'
+                + 'Pelawatte,Battaramulla.',
+                email='harini_a@parliament.lk',
+            )],
         ]:
             html_file = f'tests/test_examples/mem_real/{url_num}.html'
             html = filex.read(html_file)
