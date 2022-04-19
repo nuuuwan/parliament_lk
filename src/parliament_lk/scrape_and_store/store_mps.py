495968f751316588a4584f7e86d27e854563f8a2
nuuuwan <nuuuwan@gmail.com>
2022-04-19 16:52:10 +0530

    Improved file processing

--- src/parliament_lk/scrape_and_store/store_mps.py
+++ src/parliament_lk/scrape_and_store/store_mps.py
@@ -1,9 +1,9 @@
 import os
 
-from utils import jsonx, timex, www
+from utils import jsonx, timex, tsv, www
 
 from parliament_lk._utils import log
-from parliament_lk.scrape_and_store import scrape_mp, scrape_mp_dir
+from parliament_lk.scrape_and_store import scrape_mp, scrape_mp_idx
 from utils_future.gitx import Git
 
 URL_GIT = 'https://github.com/nuuuwan/parliament_lk'
@@ -31,39 +31,54 @@ def git_upload(git):
     git.stage_commit_and_push(message)
 
 
-def store_mp(mp_info):
-    url_num = mp_info['url_num']
+def scrape_and_store_mp_idx():
+    mp_idx_file = os.path.join(DIR_GIT_DATA, 'mp_idx.tsv')
+    if os.path.exists(mp_idx_file):
+        return tsv.read(mp_idx_file)
+
+    mp_idx_info_list = scrape_mp_idx.scrape_all_indices()
+    tsv.write(mp_idx_file, mp_idx_info_list)
+    log.info(f'Stored {mp_idx_file}')
+    return mp_idx_info_list
+
+
+def scrape_and_store_mp(url_num):
     mp_info_file = os.path.join(DIR_MP_INFO, f'{url_num}.json')
+    if os.path.exists(mp_info_file):
+        return jsonx.read(mp_info_file)
+
+    mp_info = scrape_mp.scrape(url_num)
     jsonx.write(mp_info_file, mp_info)
     log.info(f'Stored mp {url_num} to {mp_info_file}')
+    return mp_info
 
 
-def store_image(mp_info):
+def download_and_store_image(mp_info):
     url_num = mp_info['url_num']
     image_file = os.path.join(DIR_MP_IMAGES, f'{url_num}.jpg')
+    if os.path.exists(image_file):
+        return
 
-    if not os.path.exists(image_file):
-        image_url = mp_info['image_url']
-        www.download_binary(image_url, image_file)
+    image_url = mp_info['image_url']
+    www.download_binary(image_url, image_file)
 
 
 def store_all():
     git = git_download()
+    mp_idx_info_list = scrape_and_store_mp_idx()
 
-    mp_dir_info_list = scrape_mp_dir.scrape_all()
-    n_mps = len(mp_dir_info_list)
-    for i, info in enumerate(mp_dir_info_list):
+    n_mps = len(mp_idx_info_list)
+    for i, info in enumerate(mp_idx_info_list):
         log.debug(f'{i}/{n_mps}')
         url_num = info['url_num']
-        mp_info = scrape_mp.scrape(url_num)
-        store_mp(mp_info)
-        store_image(mp_info)
+        mp_info = scrape_and_store_mp(url_num)
+        download_and_store_image(mp_info)
+
         if i % GIT_UPLOAD_FREQUENCY == 0:
             git_upload(git)
-        break
 
     git_upload(git)
-    git.cleanup()
+    # git.cleanup()
 
 
 if __name__ == '__main__':
