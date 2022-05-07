cea091eceb147f57f8396d4230c24627dc6c3ebd
nuuuwan <nuuuwan@gmail.com>
2022-05-07 09:25:57 +0530

    Added date_of_birth derivations

--- src/parliament_lk/scrape_and_store/expand_mps.py
+++ src/parliament_lk/scrape_and_store/expand_mps.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+from gig import ents
 from utils import jsonx, timex
 
 from parliament_lk._utils import log
@@ -35,6 +36,9 @@ EXPANDED_MP_LIST_JSON_FILE = os.path.join(
 #   Cabinet Minister 3. 10 years as a Member of
 #   Uva Provincial Council. 4. 4 1/2 years as a Member of Parliament."
 # },
+
+
+ED_INDEX = ents.get_entity_index('ed')
 
 
 def git_upload(git):
@@ -83,11 +87,54 @@ def parse_party_short(party):
     return party.split('(')[1].split(')')[0]
 
 
+def search_ed(electoral_district):
+    electoral_district = {
+        'Nuwara - Eliya': 'Nuwara-Eliya',
+        'Mahanuwara': 'Kandy',
+        'Monaragala': 'Moneragala',
+    }.get(electoral_district, electoral_district)
+
+    for ed in ED_INDEX.values():
+        if ed['name'] == electoral_district:
+            return ed
+
+    log.error('Can not find ed for: ' + electoral_district)
+    return None
+
+
+def parse_ed_info(electoral_district):
+    matched_ed = search_ed(electoral_district)
+    return matched_ed['ed_id'], matched_ed['name']
+
+
+def parse_date_of_birth(date_of_birth):
+    date_of_birth_ut = timex.parse_time(date_of_birth, '%d-%m-%Y')
+    date_of_birth_norm = timex.format_time(date_of_birth_ut, '%Y-%m-%d')
+    return date_of_birth_ut, date_of_birth_norm
+
+
 def expand_single_mp(mp):
     name_cleaned = parse_name_cleaned(mp['name'])
     gender = parse_gender(mp['name'])
     first_names, last_name = parse_first_and_last_names(name_cleaned)
     party_short = parse_party_short(mp['party'])
+
+    date_of_birth = mp['date_of_birth']
+    if not date_of_birth:
+        date_of_birth = {
+            1575: "27-04-1951",  # Basil Rohana Rajapaksa
+            3451: "01-10-1971",  # Jagath Kumara Sumithraarachchi
+            3364: "07-10-1974",  # Lalith Varna Kumara
+        }.get(mp['url_num'])
+
+    if not date_of_birth:
+        log.error(str(mp['url_num']) + ' ' + mp['name'])
+    date_of_birth_ut, date_of_birth_norm = parse_date_of_birth(date_of_birth)
+
+    if mp['electoral_district'] == 'National List':
+        ed_id, ed_name = 'LK', 'National List'
+    else:
+        ed_id, ed_name = parse_ed_info(mp['electoral_district'])
 
     return dict(
         url_num=mp['url_num'],
@@ -106,8 +153,13 @@ def expand_single_mp(mp):
         party_short=party_short,
 
         electoral_district=mp['electoral_district'],
+        ed_id=ed_id,
+        ed_name=ed_name,
 
-        date_of_birth=mp['date_of_birth'],
+        date_of_birth=date_of_birth,
+        date_of_birth_ut=date_of_birth_ut,
+        date_of_birth_norm=date_of_birth_norm,
+
         civil_status=mp['civil_status'],
         religion=mp['religion'],
         profession=mp['profession'],
@@ -131,9 +183,14 @@ def expand_mps():
 
     # analysis only
     subset_list = sorted(list(map(
-        lambda mp: [mp['party_short'], mp['party']],
+        lambda mp: [
+            mp['date_of_birth_norm'],
+            mp['date_of_birth_ut'],
+            mp['date_of_birth'],
+        ],
         expanded_mp_list,
     )), key=lambda x: x[0])
+
     x0_to_list = {}
     for x in subset_list:
         x0 = x[0]
