84802df1f7f5520d87a17f2d8e37cf16546fba86
nuuuwan <nuuuwan@gmail.com>
2022-05-08 07:44:24 +0530

    More logging; Various updates

--- src/parliament_lk/i18n/build_dictionary.py
+++ src/parliament_lk/i18n/build_dictionary.py
@@ -1,11 +1,13 @@
+import os
 from deep_translator import GoogleTranslator
 from utils import filex
 from utils.cache import cache
 
 WORDLIST_FILE = 'src/parliament_lk/i18n/word_list.txt'
+SOURCE_LANG = 'english'
 TARGET_LANG = 'tamil'
 
-translator = GoogleTranslator(source='english', target=TARGET_LANG)
+translator = GoogleTranslator(source=SOURCE_LANG, target=TARGET_LANG)
 
 
 @cache("build_dictionary-" + TARGET_LANG, 86400 * 90)
@@ -19,9 +21,19 @@ def main():
         filex.read(WORDLIST_FILE).split('\n'),
     )))
 
-    for word in word_list:
-        word_target = translate(word)
-        print(f"'{word}': '{word_target}',")
+    var_name = f'{TARGET_LANG}_DICTIONARY'.upper()
+    log_lines = [f'const {var_name} = ' + '{']
+    for word_source in word_list:
+        word_target = translate(word_source)
+        print(word_source, '->', word_target)
+        log_lines.append(f"    '{word_source}': '{word_target}',")
+    log_lines.append('};')
+    log_lines.append(f'export default {var_name};')
+
+    log_file = f'/tmp/{TARGET_LANG}.txt'
+    filex.write(log_file, '\n'.join(log_lines))
+    print(f'Wrote to {log_file}')
+    os.system(f'open -a atom {log_file}')
 
 
 if __name__ == '__main__':
