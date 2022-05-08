8e59bcae4b1cba1a3fc460152ce6e34cba5fb3b7
nuuuwan <nuuuwan@gmail.com>
2022-05-08 11:01:10 +0530

    Script to copy images

---
+++ workflows/copy_images.py
@@ -0,0 +1,27 @@
+import os
+from utils import logx
+from utils_future.gitx import Git
+from parliament_lk._constants import URL_GIT
+
+DIR_GIT_DATA = '/tmp/parliament_lk.data'
+DIR_IMAGES = os.path.join(DIR_GIT_DATA, 'mp_images')
+DIR_JS_IMAGES = os.path.join(
+    '/Users/nuwan.senaratna/Not.Dropbox',
+    '_CODING/js_react',
+    'parliament_lk_app',
+    'public',
+    'mp_images',
+)
+
+log = logx.get_logger('copy_images')
+
+
+def main():
+    DIR_GIT_DATA = '/tmp/parliament_lk.data'
+    git = Git(URL_GIT, 'data', DIR_GIT_DATA)
+    git.clone_and_checkout()
+    os.system(f'cp {DIR_IMAGES}/* {DIR_JS_IMAGES}/')
+
+
+if __name__ == '__main__':
+    main()
