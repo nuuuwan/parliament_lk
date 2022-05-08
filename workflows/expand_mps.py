ea16f37baae208630c8e7460f1074c8e49c66540
nuuuwan <nuuuwan@gmail.com>
2022-05-08 07:54:08 +0530

    Fixed PROD_MODE bug

--- workflows/expand_mps.py
+++ workflows/expand_mps.py
@@ -5,10 +5,10 @@ from parliament_lk.scrape_and_store import expand_mps
 
 def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('prod_mode', type=bool)
+    parser.add_argument('mode', type=str)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     options = get_options()
-    expand_mps.expand_mps(options.prod_mode)
+    expand_mps.expand_mps(options.mode == 'PROD')
