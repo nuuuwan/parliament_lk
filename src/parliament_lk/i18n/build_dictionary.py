be477a2fbb42401dfe23ff01b7c6800a7229870c
nuuuwan <nuuuwan@gmail.com>
2022-05-08 09:36:57 +0530

    Various

--- src/parliament_lk/i18n/build_dictionary.py
+++ src/parliament_lk/i18n/build_dictionary.py
@@ -1,40 +1,80 @@
 import os
 
 from deep_translator import GoogleTranslator
-from utils import filex
+from utils import filex, timex
 from utils.cache import cache
 
-WORDLIST_FILE = 'src/parliament_lk/i18n/word_list.txt'
+DIR_JS_BASE = os.path.join(
+    '/Users/nuwan.senaratna/Not.Dropbox',
+    '_CODING/js_react',
+    'parliament_lk_app',
+    'src/base',
+)
+
+WORDLIST_FILE = os.path.join(
+    DIR_JS_BASE,
+    'WORD_LIST.txt',
+)
 SOURCE_LANG = 'english'
-TARGET_LANG = 'tamil'
-
-translator = GoogleTranslator(source=SOURCE_LANG, target=TARGET_LANG)
+TARGET_LANGS = ['sinhala', 'tamil']
 
 
-@cache("build_dictionary-" + TARGET_LANG, 86400 * 90)
-def translate(word):
-    return translator.translate(word)
+HARD_CODED_CHANGES = {
+    'A. Level': 'Advanced Level',
+    'ISCED7 Masters': 'ISCED7 Masters Degree',
+    'Vote for 20th Amendment': 'Voting for 20th Amendment',
+    'Voted: In Favour': 'Voted In Favour',
+    'App & Visualization by': 'App & Visualization',
+    'Data from': 'Data',
+}
 
 
 def main():
-    word_list = sorted(list(filter(
+    word_list = sorted(list(set(filter(
         lambda x: x,
         filex.read(WORDLIST_FILE).split('\n'),
-    )))
+    ))))
 
-    var_name = f'{TARGET_LANG}_DICTIONARY'.upper()
-    log_lines = [f'const {var_name} = ' + '{']
-    for word_source in word_list:
-        word_target = translate(word_source)
-        print(word_source, '->', word_target)
-        log_lines.append(f"    '{word_source}': '{word_target}',")
-    log_lines.append('};')
-    log_lines.append(f'export default {var_name};')
+    for target_lang in TARGET_LANGS:
+        translator = GoogleTranslator(source=SOURCE_LANG, target=target_lang)
 
-    log_file = f'/tmp/{TARGET_LANG}.txt'
-    filex.write(log_file, '\n'.join(log_lines))
-    print(f'Wrote to {log_file}')
-    os.system(f'open -a atom {log_file}')
+        CACHE_NAME = "build_dictionary-" + target_lang
+        CACHE_TIMEOUT = timex.SECONDS_IN.YEAR
+
+        @cache(CACHE_NAME, CACHE_TIMEOUT)
+        def translate(word):
+            return translator.translate(word)
+
+        var_name = f'{target_lang}_DICTIONARY'.upper()
+        time_id = timex.get_time_id()
+        log_lines = [
+            f'// Auto Translated {time_id}',
+            f'const {var_name} = ' + '{',
+        ]
+        for word_source in word_list:
+            word_source_use = word_source
+            for k, v in HARD_CODED_CHANGES.items():
+                if k in word_source:
+                    word_source_use = word_source.replace(k, v)
+                    print(f'\tReplaced {k} with {v} -> {word_source_use}')
+
+            word_target = translate(word_source_use)
+            print(word_source, '->', word_target)
+            if ' ' in word_source or '-' in word_source:
+                word_source_str = f'"{word_source}"'
+            else:
+                word_source_str = word_source
+
+            log_lines.append(f'  {word_source_str}: "{word_target}",')
+        log_lines.append('};')
+        log_lines.append(f'export default {var_name};')
+
+        log_file = os.path.join(
+            DIR_JS_BASE,
+            f'{target_lang}_DICTIONARY.js',
+        )
+        filex.write(log_file, '\n'.join(log_lines))
+        print(f'Wrote to {log_file}')
 
 
 if __name__ == '__main__':
